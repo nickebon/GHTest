27fd9d6ea72637c8965660feecda8065f9499fc8
Nicolas Ebon <nicolasebon4@gmail.com>
2023-02-15 10:03:19 +1100

    Initial Commit

---
+++ GHTest/GHTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GHTestApp.swift
+//  GHTest
+//
+//  Created by Nicolas Ebon on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct GHTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
